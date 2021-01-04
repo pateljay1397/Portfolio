2ed9818576eee872d1e700cce9b40f6ee153e954
Jay Patel <pateljaypj1397@gmail.com>
2021-01-03 20:43:19 -0500

    Introduction Page

--- my-portfolio-app/src/App.js
+++ my-portfolio-app/src/App.js
@@ -1,7 +1,17 @@
+import React, { Component } from "react";
 import "./App.css";
+import Introduction from "./components/introduction";
 
-function App() {
-  return <div className="App">Hello</div>;
+class App extends Component {
+  render() {
+    return (
+      <div id="colorlib-page">
+        <div id="colorlib-main">
+          <Introduction></Introduction>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
